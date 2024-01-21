c35f5a46ca5970dcd18f4974e2be9e73f417f754
Rakshit Kumar Singh <rakshitsingh421@gmail.com>
2024-01-21 13:19:39 +0000

    added docs

--- calculation.py
+++ calculation.py
@@ -5,4 +5,17 @@ def subtract_numbers(a, b):
     return a - b
 
 def average_numbers(a):
+    """Take Average of a Number.
+
+    Parameters
+    ----------
+    a
+        Collection of Numbers.
+
+    Returns
+    -------
+    int
+        Average of the Numbers.
+
+    """
     return sum(a)/len(a)
